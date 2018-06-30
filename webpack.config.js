26ffaf2396f3b90dec3abfed21bf4caa06437293
zhaks <610938672@qq.com>
2018-07-01 01:36:46 +0800

    管理资源： 1、图片处理 2、img 和 style:background 两处进行处理

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,21 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-   module: {
-         rules: [
-               {
-                 test: /\.css$/,
-                 use: [
-                   'style-loader',
-                   'css-loader'
-                 ]
-           }
-     ]
-   }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
 };
